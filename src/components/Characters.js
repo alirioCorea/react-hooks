3d0b7ac65499f377ac9c589c8a0ef7c06762fd31
Alirio Corea <coreaalirio@gmail.com>
2022-12-19 12:23:13 -0600

    useMemo

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,10 +1,11 @@
-import { useState, useEffect,useContext } from "react"
+import { useState, useEffect,useContext, useMemo} from "react"
 import ThemeContext from '../context/ThemeContext';
 
 export default function Characters() {
 
     const [characters, setCharacters] = useState([]);
     const {theme} = useContext(ThemeContext);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         fetch("https://rickandmortyapi.com/api/character/")
@@ -12,11 +13,27 @@ export default function Characters() {
             .then(data => setCharacters(data.results));
     }, []);
 
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+    }
+
+    const filteredUsers = useMemo(() => characters.filter((user) => {
+        return user.name.toLowerCase().includes(search.toLowerCase());
+    }),[characters, search]);
+
     return (
         <div className={theme ? "characters-1" : "characters-2"}>
+            <div className="search">
+                <form>
+                    <label className="buscador">
+                        <span className="buscador-tittle">Buscar Personaje:</span>
+                        <input className="buscador-input" type="text" value={search} onChange={handleSearch} />
+                    </label>
+                </form>
+            </div>
             <div className="cards-container">
                 {
-                    characters.map(character => (
+                    filteredUsers.map(character => (
                         <div key={character.id} className="card-container">
                             <img src={character.image} alt={character.name} />
                         </div>
